3e841f9c7826332f52af641e7df4c61ab25ba290
AufaSatriatama <aufa4satriatama@gmail.com>
2026-02-13 20:41:35 +0700

    dot product

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -38,9 +38,6 @@ public class VectorUtility {
     }
     
     public double dotProduct(double[] v1, double[] v2) {
-        if (v1.length != v2.length) {
-            throw new IllegalArgumentException("Vector lengths must match");
-        }
 
         double result = 0.0;
         for (int i = 0; i < v1.length; i++) {
